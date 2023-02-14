1ceccf8f6f0364e6e754fea761fba7310743fbc4
neil <1793515184@qq.com>
2023-02-14 19:47:30 +0800

    init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "赵亚飞的博客",
+      description: "赵亚飞的博客",
+    },
+  },
+
+  theme,
+
+  shouldPrefetch: false,
+});
